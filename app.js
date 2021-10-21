a34bc01873313a36626b7c28263e00afc648136f
Jaffar Aman <amanjaffar50@gmail.com>
2021-10-21 12:08:16 +0500

    js 2nd class

---
+++ app.js
@@ -0,0 +1,181 @@
+// alert("HELLO CLASS...")
+
+
+///variables///
+
+
+
+// var name = "Jaffar Aman";
+
+// var age = 20;
+
+
+// alert(name)
+
+
+///datatypes
+// string
+// Number
+// boolean 
+// undefined
+// null
+
+
+
+
+// var Name = "Jaffar"
+
+// var firstName = "Jaffar"
+// var last_namE = "aman"
+
+// alert(firstName + last_namE)
+
+
+////MATH EXPRESSTION///
+// +
+// -
+// *
+// /
+// %
+
+// var num1 = 200;
+// var num2 = 500;
+
+// var add = num1 % num2
+
+// alert(add)
+
+
+// var result = (2-3) * 10 + 20;
+//             //-1 * 10 +20
+//             //-10 +20
+//             //10
+
+// alert(result)
+
+
+// alert(3)
+// alert(1)
+// alert(2)
+
+
+// var a = "50" ///string
+// var b = 20 ////number
+
+
+// document.write(a + b)
+
+
+
+// var name = "Jaffar"
+// var name2 = "aman"
+
+// document.write(name + name2)
+
+// var num1 = 30
+// var num2 = 50
+
+// document.write(num1 + num2)
+
+
+// var num = 10;
+
+// // var update_num = num++ //POST
+// var update_num = ++num //PRE
+
+// var num2 = num
+
+
+// alert("update_num" + update_num);
+
+// alert("num2" + num2)
+
+// var num = 20;
+
+// var update_num = --num //POST
+
+// var num2 = num
+
+
+// alert("update_num" + update_num);
+
+// alert("num2" + num2)
+
+
+
+
+// var a = 20;
+
+// var result = a++  +   a++ - a  + ++a - a--
+//             //  20   +   21  - 22 + 23  - 23
+//             //  = 19
+
+
+
+// alert(result)
+
+
+
+// var a = 15;
+
+// var result = a + a++ - a++ + a  - --a + a-- + a
+//         //    =15 + 15 - 16  +  17 - 16  + 16 + 15
+
+
+// alert(result)
+
+
+
+// var a = 16;
+// var result = a++ + a++ + a - a - --a - --a + ++a
+//             //   + 17 
+
+// alert(result)
+
+
+
+        ///user sy input///
+
+        //built in method hai user sy value ky liye///
+
+// var userName  = prompt("User Name")
+
+// document.write(userName)
+
+
+// var firstName = prompt("First Name") 
+// var lastName =  prompt("Last Name")
+
+// document.write(firstName + " " + lastName)
+
+
+var name = prompt("enter your name");
+var age = prompt("enter age")
+var number = prompt("phone number")
+var address = prompt("address")
+
+document.write("USER NAME: "  + name )
+document.write("<br> age: " + age )
+document.write("<br> number: " + number )
+document.write("<br> address: " + address )
+
+
+
+
+    ////
+    // var ?name
+    // var name = 
+
+    // full name
+    // school name
+    // class 
+    // eng 
+    // urdu
+    // isl
+    // Math
+    // science
+    // obtained marks  
+    // total mark   
+    // percentage   (obt / total) * 100
+
+
